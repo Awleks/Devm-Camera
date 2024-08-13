d7c7d8ac2d5e7c5021f7cf4adcebb8b0dabfe64b
Awleks <77419507+Awleks@users.noreply.github.com>
2024-08-12 22:50:55 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -4,13 +4,4 @@ Config.MaxDistance = 20.0
 Config.Precision = 1.0
 Config.Speed = 1.0
 
-Config.WebhookUrl = 'INSERT_WEBHOOK_HERE'
-Config.ApiKey = 'INSERT_APIKEY_HERE'
-
 Config.Permissions = true
-
-Config.AllowedSteamHexes = {
-    "steam:000000000000000",
-    "steam:000000000000000",
-    "steam:000000000000000"
-}
