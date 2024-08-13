84cbaee10e4e87928ddc47eb848aec2a8a28583b
Awleks <77419507+Awleks@users.noreply.github.com>
2024-08-12 22:51:37 -0400

    Add files via upload

---
+++ server/server_config.lua
@@ -0,0 +1,11 @@
+SvConfig = {}
+
+
+SvConfig.WebhookUrl = 'https://discord.com/api/webhooks/'
+SvConfig.ApiKey = ''
+
+SvConfig.AllowedSteamHexes = {
+    "steam:000000000000000",
+    "steam:000000000000000",
+    "steam:000000000000000"
+}
